f0a90ddfc9c7c10a6420152f6a06253d51e92c0b
beth <elizabeth.bertozzi@mail.utoronto.ca>
2022-09-20 19:42:58 +0000

    Update index.css and style.ts

--- src/style.ts
+++ src/style.ts
@@ -2,7 +2,8 @@ import { styled } from "@mui/material/styles";
 import { TableHeader } from "czifui";
 
 export const StyledTableHeader = styled(TableHeader)`
+  background: lightgray;
+  margin: 0;
   position: sticky;
   top: 0;
-  background: lightgray;
 `;
